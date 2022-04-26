11da3ea6caa3b2e3709f9ae3a5e2a35014c1ced2
FlutLab.io <>
2022-04-26 03:49:34 +0000

    Autosave

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Transaction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Add Some', home: Main());
+    return MaterialApp(debugShowCheckedModeBanner: false, title: 'Add Some', home: Main());
   }
 }
 
